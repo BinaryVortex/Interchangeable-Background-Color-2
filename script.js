3c68dc6f8ca76a519f3d35b1453d1d04b5accc0d
Disandu Perera <152260753+BinaryVortex@users.noreply.github.com>
2024-08-26 07:50:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,15 @@
-const btns = document.querySelectorAll(".btn");
+document.addEventListener("DOMContentLoaded", function () {
+  const btns = document.querySelectorAll(".btn");
+  const body = document.body;
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    number = btn.value;
-    changeBackground(number);
+  btns.forEach((btn) => {
+      btn.addEventListener("click", () => {
+          changeBackground(btn.value);
+      });
   });
-});
 
-const body = document.body;
-
-function changeBackground(number) {
-  body.className = "";
-  switch (number) {
-    case "purple":
-      body.classList.add("purple");
-      break;
-    case "blue":
-      body.classList.add("blue");
-      break;
-    case "red":
-      body.classList.add("red");
-      break;
-    case "green":
-      body.classList.add("green");
-      break;
-    case "yellow":
-      body.classList.add("yellow");
-      break;
-    case "teal":
-      body.classList.add("teal");
-      break;
-    default:
-      break;
+  function changeBackground(color) {
+      body.className = ""; // Reset existing class
+      body.classList.add(color);
   }
-}
+});
