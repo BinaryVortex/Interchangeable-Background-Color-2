7d0448079fc1d3937ebbb7417054025bdc239092
Disandu Perera <152260753+BinaryVortex@users.noreply.github.com>
2024-08-26 08:00:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const btns = document.querySelectorAll(".btn");
-  const body = document.body;
+    const btns = document.querySelectorAll(".btn");
+    const body = document.body;
 
-  btns.forEach((btn) => {
-      btn.addEventListener("click", () => {
-          changeBackground(btn.value);
-      });
-  });
+    btns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            changeBackground(btn.value);
+        });
+    });
 
-  function changeBackground(color) {
-      body.className = ""; // Reset existing class
-      body.classList.add(color);
-  }
+    function changeBackground(color) {
+        body.className = ""; // Reset existing class
+        body.classList.add(color);
+    }
 });
